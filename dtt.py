da9339fe3a937ee3993ff51c3b8bf17e611ee9e7
Juttu <69361530+Juttu@users.noreply.github.com>
2022-04-13 19:38:53 +0530

    A13

--- dtt.py
+++ dtt.py
@@ -1,5 +1,21 @@
-import time
 
+
+user_name = 'd135710'
+password = 'Itsmyvsh838+'
+api_key= 'PjVObAR5'
+feed_token = None
+token_map = None
+
+
+
+
+
+
+
+# In[22]:
+
+
+from smartapi.smartConnect import SmartConnect
 import pandas as pd
 import requests
 import datetime
@@ -9,19 +25,11 @@ from twilio.rest import Client
 from datetime import datetime as date
 import math
 
-from smartapi import SmartConnect
+
+# In[31]:
 
 
-#Change the password
-user_name = 'd135710'
-password = 'Itsmyvsh838'
-api_key= 'PjVObAR5'
-feed_token = None
-token_map = None
-
-
-
-client = Client("ACe1cd9a2445658516046b7aa3ea29594b","efbf213215107c6bd57950d4f88bf1a4")
+client = Client("AC441d9d4dd5eda86b5814b88462526242","c4d8d14dfb9b20105734e2e9094b5f6e")
 
 obj=SmartConnect(api_key=api_key)
 data = obj.generateSession(user_name,password)
@@ -29,14 +37,25 @@ print(data)
 refreshToken= data['data']['refreshToken']
 
 
+
+# In[4]:
+############################
+
 feedToken=obj.getfeedToken()
 feed_token=feedToken
 
 print(feed_token)
 
 
+# In[5]:
+
+
 userProfile= obj.getProfile(refreshToken)
 print(userProfile)
+
+
+# In[6]:
+
 
 url = 'https://margincalculator.angelbroking.com/OpenAPI_File/files/OpenAPIScripMaster.json'
 d = requests.get(url).json()
@@ -48,6 +67,7 @@ token_df = token_df.astype({'strike': float})
 token_map = token_df
 
 
+# In[7]:
 
 
 def place_order(token,symbol,qty,exch_seg,buy_sell,ordertype,price):
@@ -102,36 +122,49 @@ def getday ():
         day=date.today().strftime("%A")
         return day
 
+
+# In[11]:
+
+
 def absvalue(p):
     val = p
     val2 = math.fmod(val, 100)
     x = val - val2
+    print(x,val,val2)
     abs_val = "{:.0f}".format(x)
     abs_val=int(abs_val)
+    if val2>=50:
+        abs_val=abs_val+100
     return abs_val
 
+
+# In[12]:
+
+
 def msg(m):
-        message=client.messages.create(body=m,from_="whatsapp:+14155238886",to="whatsapp:+916303247774")
+        message=client.messages.create(body=m,from_="whatsapp:+14155238886",to="whatsapp:+917386982737")
+
+
+# In[16]:
 
 
 peinfo1 = getTokenInfo('NFO','OPTIDX','NIFTY',18550,'PE').iloc[0]
 pp=getpremium(peinfo1['symbol'],peinfo1['token'],'NFO')
-print(pp)
+pp
 
-# a=0
-# while True:
-#     a=a+2
-#     print(a)
-#     msg("Hello"+str(a))
-#     time.sleep(1)
+
+# In[98]:
+
 
 while True:
             
     now = date.now()
-    if now.hour!=9 and now.minute>=30:
+    if now.hour!=4 and now.minute>=2:
 
         sp=getpremium('BANKNIFTY','26009','NSE')
         asp=absvalue(sp)
+        print("strike",asp)
+        msg("Strike"+str(asp))
         
         
         
@@ -141,7 +174,7 @@ while True:
         pp=getpremium(peinfo['symbol'],peinfo['token'],'NFO')
         
         
-        print(ceinfo,peinfo)
+        print(ceinfo,peinfo,cp,pp)
         symbol  = ceinfo['symbol']
         token = ceinfo['token']
         lot = int(ceinfo['lotsize'])
@@ -153,6 +186,11 @@ while True:
         pe_order=place_order(peinfo['token'],peinfo['symbol'],lot,'NFO','SELL','MARKET',0)
         
         print(ce_order,pe_order)
+        print(symbol,symbol1,"Strikes")
+
+        msg(symbol)
+        msg(symbol1)
+
         c=1
         p=1
 
@@ -182,11 +220,13 @@ while True:
 
                 new_cp=getpremium(ceinfo['symbol'],ceinfo['token'],'NFO')
                 p=0
-            if now.hour==15 or getpremium(ceinfo['symbol'],ceinfo['token'],'NFO')<=cp_tp or getpremium(peinfo['symbol'],peinfo['token'],'NFO')<=pp_tp:
+            if now.hour==10 or getpremium(ceinfo['symbol'],ceinfo['token'],'NFO')<=cp_tp or getpremium(peinfo['symbol'],peinfo['token'],'NFO')<=pp_tp:
                 place_order(ceinfo['token'],ceinfo['symbol'],lot,'NFO','BUY','MARKET',0)
                 place_order(peinfo['token'],peinfo['symbol'],lot,'NFO','BUY','MARKET',0)
                 profit_cp=(cp-getpremium(ceinfo['symbol'],ceinfo['token'],'NFO'))*25
                 profit_pp=(pp-getpremium(peinfo['symbol'],peinfo['token'],'NFO'))*25
+                msg(str(profit_cp))
+                msg(str(profit_pp))
 
 
                 c=2
@@ -208,7 +248,7 @@ while True:
         
         
     else:
-        print("Wait")
+        print("Wait",now)
         
 
     
@@ -216,42 +256,50 @@ while True:
 if c==0:
     
     print("Call Option Exited")
+    msg("Call Option Exited")
+
     
     while True:
         tsl_pp=new_pp*1.2
-        ttp_pp=new_pp*0.92
+        ttp_pp=new_pp*0.9
         now = date.now()
 
         if getpremium(peinfo['symbol'],peinfo['token'],'NFO')<=ttp_pp :
             new_pp=getpremium(peinfo['symbol'],peinfo['token'],'NFO')
-            print("PUT New TP,SL :",new_pp*0.92,new_pp,new_pp*1.2)
-        if getpremium(peinfo['symbol'],peinfo['token'],'NFO')>=tsl_pp or new_pp<=0.5*pp or now.hour==15:
+            print("PUT New TP,SL :",new_pp*0.9,new_pp,new_pp*1.2)
+            msg("PUT New TP : "+str(new_pp*0.9)+"Trigger"+str(new_pp)+"SL : "+str(new_pp*1.2))
+        if getpremium(peinfo['symbol'],peinfo['token'],'NFO')>=tsl_pp or new_pp<=0.5*pp or now.hour==10:
             pp_amount=(pp-getpremium(peinfo['symbol'],peinfo['token'],'NFO'))*25
             place_order(peinfo['token'],peinfo['symbol'],lot,'NFO','BUY','MARKET',0)
             print('Total P&L :',loss_cp+pp_amount)
+            msg("Total P&L :"+str(loss_cp+pp_amount))
 
             break
             
 if p==0:
     
     print("Put Option Exited")
+    msg("Put Option Exited")
 
     
     
     while True:
         tsl_cp=new_cp*1.2
-        ttp_cp=new_cp*0.92
+        ttp_cp=new_cp*0.9
         now = date.now()
 
         
         if getpremium(ceinfo['symbol'],ceinfo['token'],'NFO')<=ttp_cp :
             new_cp=getpremium(ceinfo['symbol'],ceinfo['token'],'NFO')
-            print("CALL New TP,SL :",new_cp*0.92,new_cp,new_cp*1.2)
+            print("CALL New TP,SL :",new_cp*0.9,new_cp,new_cp*1.2)
+            msg("CALL New TP : "+str(new_cp*0.9)+"Trigger"+str(new_cp)+"SL : "+str(new_cp*1.2))
 
-        if getpremium(ceinfo['symbol'],ceinfo['token'],'NFO')>=tsl_cp or new_cp<=0.5*cp or now.hour==15 :
+
+        if getpremium(ceinfo['symbol'],ceinfo['token'],'NFO')>=tsl_cp or new_cp<=0.5*cp or now.hour==10 :
             cp_amount=(cp-getpremium(ceinfo['symbol'],ceinfo['token'],'NFO'))*25
             place_order(ceinfo['token'],ceinfo['symbol'],lot,'NFO','BUY','MARKET',0)
             print('Total P&L :',loss_pp+cp_amount)
+            msg("Total P&L :"+str(loss_pp+cp_amount))
             
             break
         
@@ -263,4 +311,13 @@ if p==0:
         
         
         
+        
+        
+        
+
+
+# In[ ]:
+
+
+
 
